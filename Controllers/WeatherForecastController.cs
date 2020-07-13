ffd73b1a5aa0131eab083932c80f4591638fc9ea
Italo Di Renzo <italodirenzo93@gmail.com>
2020-07-13 13:31:55 -0400

    Feature/linting resharper (#5)
    * Configure linting rules with DotSettings
    
    * Add .editorconfig

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,12 +1,14 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace DotnetApiScaffold.Controllers
 {
+    /// <summary>
+    /// Weather Forecast related actions
+    /// </summary>
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
@@ -18,21 +20,29 @@ namespace DotnetApiScaffold.Controllers
 
         private readonly ILogger<WeatherForecastController> _logger;
 
+        /// <summary>
+        /// WeatherForecastController constructor
+        /// </summary>
+        /// <param name="logger">A logger to write to</param>
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
             _logger = logger;
         }
 
+        /// <summary>
+        /// Returns the top 5 weather forecast hits for the specified region
+        /// </summary>
+        /// <returns>Top 5 weather forecasts</returns>
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-                {
-                    Date = DateTime.Now.AddDays(index),
-                    TemperatureC = rng.Next(-20, 55),
-                    Summary = Summaries[rng.Next(Summaries.Length)]
-                })
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
                 .ToArray();
         }
     }
