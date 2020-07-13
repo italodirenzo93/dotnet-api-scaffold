b95c0b628f45e72f33a9695d86cacbdeda6c66a2
Italo Di Renzo <italodirenzo93@gmail.com>
2020-07-13 11:55:28 -0400

    Configure linting rules with DotSettings

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace DotnetApiScaffold.Controllers
 {
+    /// <summary>
+    /// Weather Forecast related actions
+    /// </summary>
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
@@ -18,11 +20,19 @@ namespace DotnetApiScaffold.Controllers
 
         private readonly ILogger<WeatherForecastController> _logger;
 
+        /// <summary>
+        /// WeatherForecastController constructor
+        /// </summary>
+        /// <param name="logger">A logger to write to</param>
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
             _logger = logger;
         }
 
+        /// <summary>
+        /// Returns the top 5 weather forecast hits for the specified region
+        /// </summary>
+        /// <returns>Top 5 weather forecasts</returns>
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
